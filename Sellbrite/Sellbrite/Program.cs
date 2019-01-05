a3910617c099f57af603c5e6d8a2099a9be7853a
joeriddles <joeriddles10@gmail.com>
2019-01-05 10:55:07 -0800

    Add Sellbrite project

---
+++ Sellbrite/Sellbrite/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sellbrite
+{
+	class Program
+	{
+		private static void Main(string[] args)
+		{
+			SellbriteClient client = new SellbriteClient();
+			client.GetWarehouses();
+		}
+	}
+}
---
+++ Sellbrite/Sellbrite/SellbriteClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Sellbrite
+{
+	public class SellbriteClient
+	{
+		private readonly string baseUrl = "https://api.sellbrite.com/v1/";
+		private readonly string apiToken = "";
+		private readonly string apiSecret = "";
+		private HttpClient Client { get; }
+
+		public SellbriteClient()
+		{
+			HttpClientHandler handler = new HttpClientHandler();
+			Client = new HttpClient(handler) { BaseAddress = new Uri(baseUrl) };
+			Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			var authByteArray = Encoding.ASCII.GetBytes($"{apiToken}:{apiSecret}");
+			Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authByteArray));
+		}
+
+		public void GetWarehouses()
+		{
+			var response = Client.GetAsync("warehouses").Result;
+			Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+		}
+	}
+}
